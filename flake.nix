05602179a4e3ce0b4228bc9db79be41122f56bd2
awwyushh <awwwyushh@gmail.com>
2025-09-08 21:01:52 +0530

    Added Home manager

--- flake.nix
+++ flake.nix
@@ -2,15 +2,27 @@
   description = "Ayush Flakes";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = {self, nixpkgs, ...}:
+  outputs = {self, nixpkgs, home-manager, ...}:
     let 
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       ayush = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [./configuration.nix];
+      };
+    };
+    homeConfigurations = {
+      ayush = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [./home.nix];
       };
     };
   };
